c2d987403be2ac584e972e6b8c540aee9c529944
kabane <kabaneshi@gmail.com>
2020-05-01 17:41:45 +0900

    微修正

--- js/code2-type.js
+++ js/code2-type.js
@@ -2,7 +2,7 @@ let str = "This is string"
 let str2 = "This is another string"
 
 console.log(str.toUpperCase()) // # => THIS IS STRING
-console.log(str2.toUpperCase()) // # => THIS IS ANOTHER STRINGs str.upcase 
+console.log(str2.toUpperCase()) // # => THIS IS ANOTHER STRING
 
 let num = 123
 console.log(num.toUpperCase()) // TypeError: num.toUpperCase is not a function
